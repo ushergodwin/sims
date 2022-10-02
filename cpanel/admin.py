faeaf197ae77f9f32f09fc4b994ab6a793c10a4f
ushergodwin <godwintumuhimbise96@gmail.com>
2022-10-02 20:39:00 +0300

    models and site admin

--- cpanel/admin.py
+++ cpanel/admin.py
@@ -1,3 +1,60 @@
+from datetime import datetime
 from django.contrib import admin
-
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django import forms
+from cpanel.models import Admission, AttendenceRegister, LogBook, PunishmentBook, Student, VistorsBook
 # Register your models here.
+
+class SimsAdmin(admin.AdminSite):
+    site_header = "SIMS ADMINISTRATION"
+    site_title = "ADMIN"
+    index_title= "SIMS"
+
+sims_admin = SimsAdmin(name='sims')
+sims_admin.register(Group, GroupAdmin)
+sims_admin.register(User, UserAdmin)
+
+class StudentChoicesField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return f"{obj.sur_name} {obj.first_name}"
+@admin.register(Student, site=sims_admin)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('sur_name', 'first_name', 'date_of_birth', 'std_no', 'sex', 'age', 'address_of_parents')
+    list_display_links = ('sur_name',)
+    search_fields = ['sur_name', 'first_name', 'std_no']
+
+@admin.register(Admission, site=sims_admin)
+class AdimissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student_name', 'age', 'class_level', 'admission_date', 'withdrawal_reason')
+    search_fields = ['id', 'class_level']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'student':
+            return StudentChoicesField(queryset=Student.objects.all())
+        return super().formfield_for_foreignkey(db_field, request, **kwargs) 
+
+    def student_name(self, obj):
+        return f"{ obj.student.sur_name } { obj.student.first_name }"
+
+    def age(self, obj):
+        current_yr = datetime.now().year
+        stud_yr = str(obj.student.date_of_birth)[:4]
+        return f"{ current_yr - int(stud_yr)}"
+@admin.register(VistorsBook, site=sims_admin)
+class VisitorsBooKAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'purpose_of_visit', 'remarks', 'date_visited')
+
+@admin.register(LogBook)
+class LogBookAdmin(admin.ModelAdmin):
+    list_display = ('log_date', 'events_reported', 'signature')
+
+@admin.register(PunishmentBook, site=sims_admin)
+class PunishmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'sex', 'class_of_offender', 'offence', 'punishment', 'by_whom', 'punishment_date')
+    list_filter = ('offence', 'punishment')
+    search_fields = ['offense', 'punishment']
+
+@admin.register(AttendenceRegister, site=sims_admin)
+class AttendenceRegisterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'admission_number', 'day', 'attended')
--- cpanel/models.py
+++ cpanel/models.py
@@ -1,3 +1,56 @@
+import uuid
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+CLASS_CHOICES = (
+        ('s1', 'S.1'), ('s2', 'S.2',), ('s3', 'S.3',),  ('s4', 'S.4'), ('s5', 'S.5'), ('s6', 'S.6')
+    )
+GENDER_CHOICES = (('F', 'Female'), ('M', 'Male'))
+DAYS_CHOICES = (('MON', 'MONDAY'), ('TUE', 'TUESDAY'), ('WED', 'WEDNESDAY'), ('THUR', 'THURSDAY'), ('FRI', 'FRIDAY'))
+ATTENDENCE_CHOICES = (('yes', 'YES'), ('no', 'NO'))
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    sur_name = models.CharField(max_length=30, verbose_name="Sur Name")
+    date_of_birth = models.DateField(verbose_name="Date of Birth")
+    std_no = models.UUIDField(verbose_name="Student Number", default=uuid.uuid4, editable=False)
+    sex = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    age = models.PositiveIntegerField()
+    address_of_parents = models.CharField(max_length=100)
+
+class Admission(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, related_name="student_admission_foregin")
+    admission_date = models.DateField()
+    class_level = models.CharField(verbose_name="Class", choices=CLASS_CHOICES, max_length=3)
+    withdrawal_reason = models.TextField(default="N/A", blank=True)
+
+
+class VistorsBook(models.Model):
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
+    purpose_of_visit = models.TextField()
+    remarks = models.TextField(blank=True, null=True)
+    date_visited = models.DateTimeField(auto_now_add=True)
+
+
+class LogBook(models.Model):
+    log_date = models.DateField(verbose_name="Date", auto_now_add=True)
+    events_reported = models.TextField()
+    signature = models.ForeignKey(User, on_delete=models.CASCADE, related_name="logbook_user_id")
+
+class PunishmentBook(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    sex = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    age = models.PositiveIntegerField()
+    class_of_offender = models.CharField(max_length=3, choices=CLASS_CHOICES)
+    offence = models.TextField()
+    punishment = models.TextField()
+    by_whom = models.CharField(max_length=30)
+    remarks = models.CharField(max_length=100, blank=True, null=True)
+    punishment_date = models.DateField(auto_now_add=True)
+
+class AttendenceRegister(models.Model):
+    admission_number = models.ForeignKey(Admission, on_delete=models.CASCADE)
+    name = models.ForeignKey(Student, on_delete=models.CASCADE)
+    day = models.CharField(max_length=10, choices=DAYS_CHOICES)
+    attended= models.CharField(max_length=10, choices=ATTENDENCE_CHOICES)
